74d09a314ce342b6f6999d63604fcf7b5be98793
Ceejay-777 <covenantcrackslord01@gmail.com>
2025-04-01 15:28:05 +0100

    About us

--- src/components/Metric.jsx
+++ src/components/Metric.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Metric = ({ perc, info }) => {
   return (
-    <div>
+      <div className={`flex flex-col gap-4 justify-center items-center w-1/5 h-[${perc}%]`}>
       <div
-        className={`h-[${perc}%] border-t-8 border-[#01628F] bg-[#01628F33]`}
+        className={`border-t-8 border-[#01628F] bg-[#01628F33] h-full w-full`}
       ></div>
       <p className="text-xl text-[#808080]">{info}</p>
     </div>
--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import Metric from "../components/Metric";
 
 const About = () => {
   return (
     <section className="w-screen px-16 py-12 bg-white">
       <div className="border flex gap-24">
-        <h2 className="text-[64px] font-semibold w-full leading-tight mt-12">About us</h2>
+        <h2 className="text-[64px] font-semibold w-full leading-tight mt-12">
+          About us
+        </h2>
         <div className="max-w-[655px]">
           <p className="text-[18px] font-semibold mb-4">
             At Learnexo, we are passionate about empowering individuals through
@@ -21,6 +24,15 @@ const About = () => {
             We are driven by a shared vision: to break down barriers to
             education and inspire learners to reach their full potential.
           </p>
+        </div>
+      </div>
+      <div className="border">
+        <div className="h-[420px] flex gap-10">
+          <Metric perc={90} info="Sustainability" />
+          <Metric perc={50} info="Customer’s Satisfaction" />
+          <Metric perc={50} info="Our Services" />
+          <Metric perc={50} info="Improved Grades" />
+          <Metric perc={50} info="Improved WAEC Results" />
         </div>
       </div>
     </section>
